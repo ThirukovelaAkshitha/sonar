b58374c9774cbf63b7a97c0061b879b7c37e0be9
ThirukovelaAkshitha <93242638+ThirukovelaAkshitha@users.noreply.github.com>
2021-11-09 11:51:43 +0530

    Update ProductController.java

--- src/main/java/com/orderManagement/controller/ProductController.java
+++ src/main/java/com/orderManagement/controller/ProductController.java
@@ -2,7 +2,7 @@ package com.orderManagement.controller;
 
 import java.util.List;
 
-import javax.validation.Valid;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
